d3cb70316dbf309e462044d3fdf3ef603608f641
Davidson-LG <136202466+Davidson-LG@users.noreply.github.com>
2025-04-28 14:02:52 -0300

    Update download_anbima.py

--- download_anbima.py
+++ download_anbima.py
@@ -1,4 +1,5 @@
 # download_anbima.py
+pip install gspread google-auth google-auth-oauthlib google-auth-httplib2
 import os
 import time
 from datetime import datetime
@@ -84,3 +85,38 @@ for nome_exibicao, valor_select, nome_arquivo in indices:
 
 driver.quit()
 print("Todos downloads concluídos!")
+
+import gspread
+from google.oauth2.service_account import Credentials
+import os
+
+# Autenticar
+scopes = ["https://www.googleapis.com/auth/drive"]
+credentials = Credentials.from_service_account_file('credentials.json', scopes=scopes)
+gc = gspread.authorize(credentials)
+
+# ID da pasta do Drive
+folder_id = 'https://drive.google.com/drive/folders/1Q-wo4KFvGIZEEe9PoTMt_TPUK9Kuww_e?usp=drive_link'
+
+from googleapiclient.discovery import build
+service = build('drive', 'v3', credentials=credentials)
+
+# Upload dos arquivos
+def upload_to_drive(filepath, folder_id):
+    filename = os.path.basename(filepath)
+    file_metadata = {
+        'name': filename,
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(filepath, resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(f'Arquivo {filename} enviado para o Google Drive.')
+
+from googleapiclient.http import MediaFileUpload
+
+# Enviar todos arquivos da pasta downloads/
+downloads_path = 'downloads'
+for file in os.listdir(downloads_path):
+    if file.endswith('.csv'):
+        upload_to_drive(os.path.join(downloads_path, file), folder_id)
+
